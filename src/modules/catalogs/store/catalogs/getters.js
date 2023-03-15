2859f4050445ac0c59055903559fea60754b59ec
MagicalPrograms <brian.sauceda.ca12@gmail.com>
2023-03-15 15:34:38 -0700

    Solucion de bug en el buscador

--- src/modules/catalogs/store/catalogs/getters.js
+++ src/modules/catalogs/store/catalogs/getters.js
@@ -1,4 +1,4 @@
 export const getRequestByTerm = ( state ) => ( term = '' ) => {
     if ( term.length === 0 ) return state.requests
-    return state.requests.filter( reques => reques.id.toLowerCase().includes(term.toLocaleLowerCase()) || reques.Description.toLowerCase().includes(term.toLocaleLowerCase()) || reques.Dependencie.name.toLowerCase().includes(term.toLocaleLowerCase()))
+    return state.requests.filter( reques => reques.id.toLowerCase().includes(term.toLocaleLowerCase()) || reques.Descripcion.toLowerCase().includes(term.toLocaleLowerCase()) || reques.Dependencia.name.toLowerCase().includes(term.toLocaleLowerCase()))
 }
