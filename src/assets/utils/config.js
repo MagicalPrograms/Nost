ba6b6b3d9fb52fa25e496ca62ce92a0acb7ab216
MagicalPrograms <brian.sauceda.ca12@gmail.com>
2023-03-15 15:26:50 -0700

    ok

---
+++ src/assets/utils/config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDbxtrM9bljTiJOxC87j5BdDHm5OpaNsMc",
+    authDomain: "nost-beta.firebaseapp.com",
+    databaseURL: "https://nost-beta-default-rtdb.firebaseio.com",
+    projectId: "nost-beta",
+    storageBucket: "nost-beta.appspot.com",
+    messagingSenderId: "586716879771",
+    appId: "1:586716879771:web:dbabe6b8e30d05c0bb5f3e",
+    measurementId: "G-HZ299NBY84"
+}
